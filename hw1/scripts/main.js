3214d1ee245ba2a18465a8b8332368adbb9ba4fc
Hsinyu <247047.niuitmo.ru>
2017-10-08 21:44:09 +0300

    formatting updated by VSCODE

--- hw1/scripts/main.js
+++ hw1/scripts/main.js
@@ -12,10 +12,10 @@ myButton.onclick = function() {
   myQuote = document.getElementById("quote0");	
 	//myQuote.textContent = "Lannisters Always pay their debts. - House Stark of Lanister";
   randomNum = Math.floor(Math.random() * 10)%7;  
-  randomQuoteId = "quote"+randomNum;
+  randomQuoteId = "quote" + randomNum;
   randomQuote = document.getElementById(randomQuoteId);  
   //alert(randomQuote);
-  tmp=myQuote.textContent;
+  tmp = myQuote.textContent;
   quote0.textContent = randomQuote.textContent ;
   randomQuote.textContent = tmp;
   //alert(randomQuote.textContent.length)
@@ -24,16 +24,16 @@ myButton.onclick = function() {
 myButton2.onclick = function() {	 
   for (i=1;i<8;i++){
     quoteId[i] = "quote"+i;   
-    quote[i]=document.getElementById(quoteId[i])//.textContent 
+    quote[i] = document.getElementById(quoteId[i])//.textContent 
     //alert(quote[i].length)
    }
-  //Bubble sort
+//Bubble sort
   for (j=1;j<7;j++){
     for (i=1;i<7;i++){
     if(quote[i].textContent.length > quote[i+1].textContent.length)
       {    tmp = quote[i+1].textContent;
           quote[i+1].innerHTML = quote[i].textContent;
-          quote[i].innerHTML= tmp;   
+          quote[i].innerHTML = tmp;   
       }
     }
   }
