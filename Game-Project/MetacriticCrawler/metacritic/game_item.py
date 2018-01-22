b22ec6da62877462c763f943c04483d167c608e5
Hsinyu <247047.niuitmo.ru>
2018-01-22 22:48:19 +0300

    Move game crawl here

---
+++ Game-Project/MetacriticCrawler/metacritic/game_item.py
@@ -0,0 +1,13 @@
+
+class Game:
+	
+    title = ''
+    platform = ''
+    released = ''
+    metascore = ''
+    reviews_count = ''
+    userscore = ''
+    user_count = ''
+    developer = ''
+    genres = []
+    rating = ''
